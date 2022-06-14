6e16b00da631275527194c59a50c0c4a8b515393
Armen Mirzoyan Denisov <100298950+armirzoya@users.noreply.github.com>
2022-06-14 15:33:34 +0200

    Clean module

--- src/app/management/management.module.ts
+++ src/app/management/management.module.ts
@@ -1,35 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { TableModule } from 'primeng/table'
 import { PaginatorModule } from 'primeng/paginator';
 import { TranslateModule } from '@ngx-translate/core';
-import { UsersListComponent } from './views/users-list/users-list.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { ButtonModule } from 'primeng/button';
 
+import { UsersListComponent } from './views/users-list/users-list.component';
+
 @NgModule({
   declarations: [
-    UsersListComponent,
-
+    UsersListComponent
   ],
   imports: [
     ButtonModule,
     BrowserModule,
     CommonModule,
     HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
     TableModule,
-    
     PaginatorModule,
-    TranslateModule,
-    BrowserAnimationsModule,
-    
-    
-  
+    TranslateModule
   ]
 })
 export class ManagementModule { }
