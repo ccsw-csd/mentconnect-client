cf0c687fe11c78d4fc2a6349438cb152c90a7fbd
Alejandro Pintor Correas <alejandro.pintor-correas.external@capgemini.com>
2023-03-08 13:29:37 +0100

    change the language of the text fields and eliminate the console error on login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { PrimeNGConfig } from 'primeng/api';
+import { AuthService } from 'src/app/core/services/auth.service';
 import { RoleService } from './core/services/role.service';
 
 @Component({
@@ -14,15 +15,18 @@ export class AppComponent {
 
   constructor( 
     private config: PrimeNGConfig, 
+    private auth: AuthService,
     private translateService: TranslateService, 
     private roleService: RoleService)
   {}
 
   ngOnInit(): void {
     this.translateService.get('primeng').subscribe(res => this.config.setTranslation(res));
-    
-    this.roleService.findRoles().subscribe((roles => {
-      sessionStorage.setItem("roles", JSON.stringify(roles));
-    }));
+    if(this.auth.getToken() != null){
+      this.roleService.findRoles().subscribe((roles => {
+        sessionStorage.setItem("roles", JSON.stringify(roles));
+      }));
+    }
+
   }
 }
